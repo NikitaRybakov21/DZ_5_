03037ce92f7eb2a40491bc4241db9c31398a6d96
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-09-07 19:54:33 +0300

    Dz---5---

---
+++ src/main/java/Item.java
@@ -0,0 +1,11 @@
+public class Item {
+    public int mass;
+    public int price;
+    public String name;
+
+    public Item(String name,int mass,int price){
+        this.name = name;
+        this.mass = mass;
+        this.price = price;
+    }
+}
